a65d66698806bed478b43c76f0449943e5d38559
Joe(RanZhou) <zhouranus@gmail.com>
2022-12-14 18:56:19 -0600

    changed test scripts

--- src/app/main-game/game-engin.spec.ts
+++ src/app/main-game/game-engin.spec.ts
@@ -5,6 +5,10 @@ describe('GameEngin', () => {
     expect(new GameEngin()).toBeTruthy();
   });
 
-
+  it('random player', () => {
+    const newgame = new GameEngin();
+    // @ts-ignore
+    expect(newgame.randomPlayerStart()).toBeLessThanOrEqual(2);
+  });
 
 });
